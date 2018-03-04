2e46d830029935ec926c4f52c616cd7de8e5994b
jeesquivel <esquivelsamudio95@gmail.com>
2018-03-04 14:02:33 -0600

    se hizo la pantalla de dos bottones :) te amo

--- src/controlador/VistaInicialController.java
+++ src/controlador/VistaInicialController.java
@@ -4,6 +4,7 @@ package controlador;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -16,6 +17,8 @@ import javafx.stage.Stage;
  */
 public class VistaInicialController {
 
+
+
     @FXML
     public void pressButton(ActionEvent event){
         try {
@@ -23,13 +26,12 @@ public class VistaInicialController {
             Parent root = fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
+            Stage principal= (Stage) ((Node) event.getSource()).getScene().getWindow();
+            principal.hide();
             stage.show();
-
         } catch(Exception e) {
-            //e.printStackTrace();
         }
     }
-
 
 
 }
